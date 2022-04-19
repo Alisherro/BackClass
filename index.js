a3e6e0028b2e952e9aeb7031a04585028d3d4877
Alisherro <good071104@gmail.com>
2022-04-19 15:52:32 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import path from 'path'
+import {requestTime,logger} from './middlewares.js'
+
+const __dirname=path.resolve()
+const PORT=3000
+const app=express()
+
+app.set('view engine','ejs')
+app.set('views',path.resolve(__dirname,'ejs'))
+
+
+app.use(express.static(path.resolve(__dirname,'static')))
+
+
+app.get('/',(req,res)=>{
+    res.render('index',{title:'Main page ', active:'main'})
+})
+
+
+app.get('/features',(req,res)=>{2
+    res.render('features',{title:'Features page', active:'features'})
+})
+
+app.get('/log',(req,res)=>{
+    res.render('log',{title:'Logging', active:'log'})
+})
+
+app.listen(PORT,()=>{
+    console.log("Сервер запущен на порте",PORT)
+})
+
